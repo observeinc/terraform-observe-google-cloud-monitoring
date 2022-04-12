dd0bafeb958bfc2fc60dfc560acffa1b83601435
Luke Zhu <luke@observeinc.com>
2022-04-12 22:24:26 +0000

    fixes and cloud function logs+metrics

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ resource "observe_dataset" "base_asset_inventory_records" {
       make_col data:parse_json(data)
       
       make_col time:parse_isotime(string(startTime))
-      set_valid_from time
+      set_valid_from options(max_time_diff:${var.max_time_diff}), time
 
       pick_col
         time,
@@ -119,7 +119,7 @@ resource "observe_dataset" "base_asset_inventory_records" {
       filter not is_null(data.asset_type) and not is_null(data.name)
 
       make_col time:publishTime
-      set_valid_from time
+      set_valid_from options(max_time_diff:${var.max_time_diff}), time
 
       pick_col 
         time,
@@ -149,6 +149,7 @@ resource "observe_dataset" "resource_asset_inventory_records" {
     pipeline = <<-EOF
       pick_col 
         time,
+        asset_type,
         name,
         data:object(resource.data),
         discovery_document_uri:string(resource.discovery_document_uri),
@@ -206,13 +207,14 @@ resource "observe_dataset" "logs" {
           protoPayload:object(data.protoPayload),
           jsonPayload:data.jsonPayload,
           labels:object(data.labels),
-          resource:object(data.resource),
+          resourceLabels:object(data.resource.labels),
+          resourceType:string(data.resource.type),
           httpRequest:object(data.httpRequest),
           insertId:string(data.insertId),
           trace:string(data.trace),
           messageId:id
           
-      set_valid_from timestamp
+      set_valid_from options(max_time_diff:${var.max_time_diff}), timestamp
       drop_col BUNDLE_TIMESTAMP
     EOF
   }
@@ -265,6 +267,9 @@ resource "observe_dataset" "metrics" {
 
       make_col metric:FIELDS.timeseries.metric,
         metric_kind:int64(FIELDS.timeseries.metric_kind),
+        resource_type:string(FIELDS.timeseries.resource.type),
+        resource_labels:object(FIELDS.timeseries.resource.labels),
+        value_type:int64(FIELDS.timeseries.value_type),
         points:FIELDS.timeseries.points
           
       make_col 
@@ -274,14 +279,24 @@ resource "observe_dataset" "metrics" {
       make_col start_time:timestamp_s(int64(@."_c_points_value".interval.start_time.seconds)) + duration(if_null(int64(@."_c_points_value".interval.start_time.nanos), 0)),
         end_time:timestamp_s(int64(@."_c_points_value".interval.end_time.seconds)) + duration(if_null(int64(@."_c_points_value".interval.end_time.nanos), 0)),
         value:@."_c_points_value".value
-      set_valid_from start_time
+      set_valid_from options(max_time_diff:${var.max_time_diff}), start_time
+      
+      make_col value:coalesce(
+          float64(value.Value.Int64Value),
+          float64(value.Value.DoubleValue),
+          float64(case(value.Value.BoolValue = true, 1, value.Value.BoolValue = false, 0)))
 
       pick_col
         start_time,
         end_time,
         metric_type,
+        metric_kind,
+        metric_labels,
+        resource_type,
+        resource_labels,
         value,
-        metric_labels
+        value_type
+      interface "metric", metric:metric_type, value:value
     EOF
   }
 }
@@ -300,7 +315,7 @@ resource "observe_dataset" "cloud_function" {
       make_col
         assetInventoryName:name,
         name:string(data.name)
-      make_resource
+      make_resource options(expiry:${var.max_expiry}),
         assetInventoryName,
         availableMemoryMb:int64(data.availableMemoryMb),
         buildId:string(data.buildId),
@@ -321,6 +336,159 @@ resource "observe_dataset" "cloud_function" {
         updateTime:parse_isotime(string(data.updateTime)),
         versionId:string(data.versionId),
         primary_key(name)
+      
+      extract_regex name, /projects\/(?P<projectId>[^\/+]+)\/locations\/(?P<region>[^\/+]+)\/functions\/(?P<functionName>[^\/+]+)/
+      add_key projectId, region, functionName
     EOF
+  }
+}
+
+resource "observe_dataset" "cloud_function_logs" {
+  workspace = var.workspace.oid
+  name      = format(var.name_format, "Cloud Function Logs")
+  freshness = var.freshness_default
+
+  inputs = {
+    "logs" = observe_dataset.logs.oid
+  }
+
+  stage {
+    pipeline = <<-EOF
+      filter resourceType = "cloud_function"
+
+      make_col 
+        functionName:string(resourceLabels.function_name),
+        projectId:string(resourceLabels.project_id),
+        region:string(resourceLabels.region)
+
+      pick_col 
+        timestamp,
+        receiveTimestamp,
+        logName,
+        severity,
+        textPayload,
+        projectId,
+        region,
+        functionName
+    EOF
+  }
+}
+
+resource "observe_link" "cloud_function_logs" {
+  workspace = var.workspace.oid
+  source    = observe_dataset.cloud_function_logs.oid
+  target    = each.value.target
+  fields    = each.value.fields
+  label     = each.key
+
+  for_each = {
+    "Cloud Function" = {
+      target = observe_dataset.cloud_function.oid
+      fields = ["projectId", "region", "functionName"]
+    }
+  }
+}
+
+
+locals {
+  cloud_function_metrics = {
+    "cloudfunctions.googleapis.com/function/active_instances" = {
+      type        = "gauge"
+      description = <<-EOF
+        The number of active function instances. Sampled every 60 seconds. After sampling, data is not visible for up to 240 seconds.
+      EOF
+      rollup      = "avg"
+      aggregate   = "sum"
+    },
+    "cloudfunctions.googleapis.com/function/execution_count" = {
+      type        = "delta"
+      description = <<-EOF
+        Count of function executions broken down by status. Sampled every 60 seconds. After sampling, data is not visible for up to 240 seconds.
+      EOF
+      rollup      = "rate"
+      aggregate   = "sum"
+    },
+    "cloudfunctions.googleapis.com/function/execution_times" = {
+      type        = "gauge"
+      description = <<-EOF
+        The number of active function instances. Sampled every 60 seconds. After sampling, data is not visible for up to 240 seconds.
+      EOF
+      rollup      = "avg"
+      aggregate   = "sum"
+    },
+    "cloudfunctions.googleapis.com/function/instance_count" = {
+      type        = "gauge"
+      description = <<-EOF
+        The number of active function instances. Sampled every 60 seconds. After sampling, data is not visible for up to 240 seconds.
+      EOF
+      rollup      = "avg"
+      aggregate   = "sum"
+    },
+    "cloudfunctions.googleapis.com/function/network_egress" = {
+      type        = "delta"
+      description = <<-EOF
+        Outgoing network traffic of function, in bytes. Sampled every 60 seconds. After sampling, data is not visible for up to 240 seconds.
+      EOF
+      rollup      = "rate"
+      aggregate   = "sum"
+    },
+    "cloudfunctions.googleapis.com/function/user_memory_bytes" = {
+      type        = "delta"
+      description = <<-EOF
+        Distribution of maximum function's memory usage during execution, in bytes. Sampled every 60 seconds. After sampling, data is not visible for up to 180 seconds.
+      EOF
+      rollup      = "avg"
+      aggregate   = "sum"
+    },
+  }
+}
+
+resource "observe_dataset" "cloud_function_metrics" {
+  workspace = var.workspace.oid
+  name      = format(var.name_format, "Cloud Function Metrics")
+  freshness = var.freshness_default
+
+  inputs = {
+    "metrics" = observe_dataset.metrics.oid
+  }
+
+  stage {
+    pipeline = <<-EOF
+      filter resource_type = "cloud_function"
+
+      make_col 
+        function_name:string(resource_labels.function_name),
+        project_id:string(resource_labels.project_id),
+        region:string(resource_labels.region)
+
+      pick_col
+        start_time,
+        end_time,
+        metric_type,
+        metric_kind,
+        metric_labels,
+        value,
+        value_type,
+        project_id,
+        region,
+        function_name
+
+      ${join("\n\n", [for metric, options in local.cloud_function_metrics : indent(2, format("set_metric options(\n%s\n), %q", join(",\n", [for k, v in options : k == "interval" ? format("%s: %s", k, v) : format("%s: %q", k, v)]), metric))])}
+    EOF
+  }
+}
+
+resource "observe_link" "cloud_function_metrics" {
+  workspace = var.workspace.oid
+  source    = observe_dataset.cloud_function_metrics.oid
+  target    = each.value.target
+  fields    = each.value.fields
+  label     = each.key
+
+  for_each = {
+    "Cloud Function" = {
+      target = observe_dataset.cloud_function.oid
+      fields = ["project_id:projectId", "region", "function_name:functionName"]
+    }
   }
 }
