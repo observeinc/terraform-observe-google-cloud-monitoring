4ff39f961eabe17b7f4b1d005445a42ef46b4d8c
Arthur Dayton <arthur@observeinc.com>
2022-06-17 14:01:00 -0700

    add fields to database instance

--- service/cloudsql/main.tf
+++ service/cloudsql/main.tf
@@ -21,6 +21,14 @@ resource "observe_dataset" "cloudsql" {
       make_col
         assetInventoryName:name,
         name:string(data.name)
+        make_col ipAddressObject:pivot_array( array(data.ipAddresses), "type", "ipAddress" )
+
+
+        make_col databaseFlags:data.settings.databaseFlags
+        make_col ipConfiguration:data.settings.ipConfiguration
+        make_col tier:string(data.settings.tier)
+        make_col state:string(data.state)
+
       make_resource options(expiry:${var.max_expiry}),
         name,
         databaseVersion: string(data.databaseVersion),
@@ -30,7 +38,16 @@ resource "observe_dataset" "cloudsql" {
         region:  string(data.region),
         database_id: strcat(string(data.project),":",name),
         backendType:string(data.backendType),
+        backupConfiguration:data.settings.backupConfiguration,
+        availabilityType:string(data.settings.availabilityType),
+        dataDiskSizeGb:string(data.settings.dataDiskSizeGb),
+        dataDiskType:string(data.settings.dataDiskType),
+        databaseFlags:data.settings.databaseFlags,
+        ipConfiguration:data.settings.ipConfiguration,
+        tier:string(data.settings.tier),
+        state:string(data.state),
         createTime:string(data.createTime),
+        ipAddressPrimary: ipAddressObject.PRIMARY,
         ipAddresses:string(data.ipAddresses),
         gceZone:string(data.gceZone),
         settings:string(data.settings),
