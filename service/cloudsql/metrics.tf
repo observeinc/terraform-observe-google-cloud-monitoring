08e56efb44b6fcecd2d2d334de28fba8bef138cd
Arthur Dayton <arthur@observeinc.com>
2022-06-21 16:40:58 -0700

    update metric for loop for readability

--- service/cloudsql/metrics.tf
+++ service/cloudsql/metrics.tf
@@ -32,9 +32,17 @@ resource "observe_dataset" "cloudsql_metrics" {
 
       interface "metric", metric:metric_type, value:value
       
-      ${join("\n\n", [for metric, options in local.metrics_definitions : indent(2, format("set_metric options(\n%s\n), %q", join(",\n", [for k, v in options : k == "interval" ? format("%s: %s", k, v) : format("%s: %q", k, v) if contains(var.metric_interface_fields, k)]), metric)) if contains(var.metric_launch_stages, options.launchStage)])}
+      ${join("\n\n",
+    [for metric, options in local.metrics_definitions :
+      indent(2,
+        format("set_metric options(\n%s\n), %q",
+          join(",\n",
+      [for k, v in options : k == "interval" ? format("%s: %s", k, v) : format("%s: %q", k, v) if contains(var.metric_interface_fields, k)]), metric))
+    if contains(var.metric_launch_stages, options.launchStage)]
+  )
+}
     EOF
-  }
+}
 }
 
 # use var instead of prop metric_interface_fields
