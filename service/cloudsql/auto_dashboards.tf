2eee8a7e2088191c28df10449888ad03cd3343f3
Arthur Dayton <arthur@observeinc.com>
2022-07-22 14:56:55 -0500

    fix file reference for submodules

--- service/cloudsql/auto_dashboards.tf
+++ service/cloudsql/auto_dashboards.tf
@@ -1,17 +1,17 @@
 resource "observe_dashboard" "monitoring" {
   workspace = var.workspace.oid
   name      = "Cloud SQL Instance Monitoring Output"
-  stages = templatefile("/Users/arthur/content_eng/terraform-observe-google/service/cloudsql/dashboards/stages_v1.json", {
+  stages = templatefile("${path.module}/dashboards/stages_v1.json", {
     dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
     dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
     dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
   })
-  layout = templatefile("/Users/arthur/content_eng/terraform-observe-google/service/cloudsql/dashboards/layout_v1.json", {
+  layout = templatefile("${path.module}/dashboards/layout_v1.json", {
     dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
     dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
     dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
   })
-  parameters = templatefile("/Users/arthur/content_eng/terraform-observe-google/service/cloudsql/dashboards/parameters_v1.json", {
+  parameters = templatefile("${path.module}/dashboards/parameters_v1.json", {
     dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
     dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
     dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
