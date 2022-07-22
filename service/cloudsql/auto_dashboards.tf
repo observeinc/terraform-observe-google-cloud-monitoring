269211ad0f39a15fe9e23a6d58b2eb870b285868
Arthur Dayton <arthur@observeinc.com>
2022-07-22 14:14:27 -0500

    changes for dashboard

--- service/cloudsql/auto_dashboards.tf
+++ service/cloudsql/auto_dashboards.tf
@@ -2,19 +2,19 @@ resource "observe_dashboard" "monitoring" {
   workspace = data.observe_workspace.ws.oid
   name      = "Cloud SQL Instance Monitoring Output"
   stages = templatefile("/Users/arthur/content_eng/terraform-observe-google/service/cloudsql/dashboards/stages_v1.json", {
-    dataset_gcp-101_cloudSQLInstance = "41079440"
-    dataset_gcp-101_cloudSQLMetrics  = "41079433"
-    dataset_gcp-101_cloudSQLLogs     = "41079429"
+    dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
+    dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
+    dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
   })
   layout = templatefile("/Users/arthur/content_eng/terraform-observe-google/service/cloudsql/dashboards/layout_v1.json", {
-    dataset_gcp-101_cloudSQLInstance = "41079440"
-    dataset_gcp-101_cloudSQLMetrics  = "41079433"
-    dataset_gcp-101_cloudSQLLogs     = "41079429"
+    dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
+    dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
+    dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
   })
   parameters = templatefile("/Users/arthur/content_eng/terraform-observe-google/service/cloudsql/dashboards/parameters_v1.json", {
-    dataset_gcp-101_cloudSQLInstance = "41079440"
-    dataset_gcp-101_cloudSQLMetrics  = "41079433"
-    dataset_gcp-101_cloudSQLLogs     = "41079429"
+    dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
+    dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
+    dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
   })
   # parameter_values = templatefile("${path.module}//Users/arthur/content_eng/terraform-observe-google/service/cloudsql/dashboards/parameterValues_v1.json", {
   #
