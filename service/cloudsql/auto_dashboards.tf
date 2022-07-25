f670478d56a6d6e3d52dd46006b3736f1a509a9e
Arthur Dayton <arthur@observeinc.com>
2022-07-25 09:45:00 -0500

    update to latest dashboard

--- service/cloudsql/auto_dashboards.tf
+++ service/cloudsql/auto_dashboards.tf
@@ -2,19 +2,19 @@ resource "observe_dashboard" "monitoring" {
   workspace = var.workspace.oid
   name      = "Cloud SQL Instance Monitoring Output"
   stages = templatefile("${path.module}/dashboards/stages_v1.json", {
-    dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
-    dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
-    dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
+    dataset_gcp-172_cloudSQLInstance = observe_dataset.cloudsql.id
+    dataset_gcp-172_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
+    dataset_gcp-172_cloudSQLLogs     = observe_dataset.sql_logs.id
   })
   layout = templatefile("${path.module}/dashboards/layout_v1.json", {
-    dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
-    dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
-    dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
+    dataset_gcp-172_cloudSQLInstance = observe_dataset.cloudsql.id
+    dataset_gcp-172_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
+    dataset_gcp-172_cloudSQLLogs     = observe_dataset.sql_logs.id
   })
   parameters = templatefile("${path.module}/dashboards/parameters_v1.json", {
-    dataset_gcp-101_cloudSQLInstance = observe_dataset.cloudsql.id
-    dataset_gcp-101_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
-    dataset_gcp-101_cloudSQLLogs     = observe_dataset.sql_logs.id
+    dataset_gcp-172_cloudSQLInstance = observe_dataset.cloudsql.id
+    dataset_gcp-172_cloudSQLMetrics  = observe_dataset.cloudsql_metrics[0].id
+    dataset_gcp-172_cloudSQLLogs     = observe_dataset.sql_logs.id
   })
   # parameter_values = templatefile("${path.module}//Users/arthur/content_eng/terraform-observe-google/service/cloudsql/dashboards/parameterValues_v1.json", {
   #
--- service/cloudsql/example/template_dashboard.tf
+++ service/cloudsql/example/template_dashboard.tf
@@ -1,7 +1,7 @@
 # Dashboard used to create terraform definition
 data "observe_dashboard" "db" {
   workspace = data.observe_workspace.ws.oid
-  id        = "41079648"
+  id        = "41097640"
 }
 
 data "observe_workspace" "ws" {
