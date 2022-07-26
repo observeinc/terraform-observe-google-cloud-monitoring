36ff3d4dd9aea8c10d5ed9d5c3f3857a16320409
Arthur Dayton <arthur@observeinc.com>
2022-07-26 13:59:48 -0500

    fix version references

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     observe = {
       source  = "terraform.observeinc.com/observeinc/observe"
-      version = "~> 0.6"
+      version = "~> 0.7"
     }
   }
   required_version = ">= 1.0"
