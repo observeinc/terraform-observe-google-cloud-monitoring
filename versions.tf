5800b9a0a39afce0294abd4462566defbafb7a2d
Arthur Dayton <arthur@observeinc.com>
2022-07-26 15:09:59 -0500

    make versions happy and update dashboard

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     observe = {
       source  = "terraform.observeinc.com/observeinc/observe"
-      version = "~> 0.7"
+      version = ">= 0.7.0"
     }
   }
   required_version = ">= 0.13.0"
