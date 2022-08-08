25724ed7a1f19c1348fe111b37903a7e812b1b6b
Arthur Dayton <arthur@observeinc.com>
2022-08-08 10:21:10 -0700

    latest dashboard

--- service/compute/template_dashboard/main.tf
+++ service/compute/template_dashboard/main.tf
@@ -1,7 +1,7 @@
 # Dashboard used to create terraform definition
 data "observe_dashboard" "db" {
   workspace = data.observe_workspace.ws.oid
-  id        = "41109759"
+  id        = "41111669"
 }
 
 data "observe_workspace" "ws" {
