6ee19027af2eec0f06b07bfa4684c2b308b52b33
Arthur Dayton <arthur@observeinc.com>
2022-08-12 17:04:59 -0700

    scrub ids from json

--- service/cloudsql/outputs.tf
+++ service/cloudsql/outputs.tf
@@ -14,6 +14,10 @@ output "cloudsql_metrics_combo" {
   value = local.enable_metrics ? observe_dataset.cloudsql_metrics_combo[0] : null
 }
 
+output "cloudsql_metrics_wide" {
+  value = local.enable_metrics ? observe_dataset.cloudsql_metrics_wide[0] : null
+}
+
 output "combined_error_logs" {
   value = local.enable_metrics ? observe_dataset.combined_error_logs : null
 }
