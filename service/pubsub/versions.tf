33296dc1264f2648bcbbd82480920340a168df34
Arthur Dayton <arthur@observeinc.com>
2022-08-16 07:26:08 -0700

    setup of pubsub

---
+++ service/pubsub/versions.tf
@@ -0,0 +1,10 @@
+terraform {
+  required_providers {
+    observe = {
+      source  = "terraform.observeinc.com/observeinc/observe"
+      version = ">= 0.7.0"
+    }
+  }
+  required_version = ">= 0.13.0"
+}
+
