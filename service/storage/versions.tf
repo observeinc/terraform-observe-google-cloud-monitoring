e727ab6747d2434d981dc3b76fa62e29c68207ac
Arthur Dayton <arthur@observeinc.com>
2022-08-18 09:31:43 -0700

    pin to 10.4

--- service/storage/versions.tf
+++ service/storage/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     observe = {
       source  = "terraform.observeinc.com/observeinc/observe"
-      version = "~> 0.10.4"
+      version = "= 0.10.4"
     }
   }
   required_version = ">= 0.13.0"
