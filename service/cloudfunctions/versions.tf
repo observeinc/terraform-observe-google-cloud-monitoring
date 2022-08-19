ad99d893d36ae95b47d45e91802d6f9a1fa442da
arthur-observe <103078673+arthur-observe@users.noreply.github.com>
2022-08-19 12:02:24 -0700

    chore: bump observe provider to 10.5 (#61)
    * bump observe provider to 10.5

--- service/cloudfunctions/versions.tf
+++ service/cloudfunctions/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     observe = {
       source  = "terraform.observeinc.com/observeinc/observe"
-      version = ">= 0.9.2, <= 0.10.4"
+      version = ">= 0.9.2, <= 0.10.5"
     }
   }
   required_version = ">= 0.13.0"
