2b6c4dbcb5a4e0a081dcc1eaf9ab09ab55194d60
arthur-observe <103078673+arthur-observe@users.noreply.github.com>
2022-09-14 21:12:23 -0700

    chore: change reference to field (#81)
    * change reference to field
    
    * reshaped project

--- projects.tf
+++ projects.tf
@@ -3,46 +3,59 @@ resource "observe_dataset" "projects_collection_enabled" {
   name        = format(var.name_format, "Projects Collection Enabled")
   freshness   = lookup(var.freshness_overrides, "projects", var.freshness_default)
   description = "This dataset is used to create project resources"
+
   inputs = {
-    "events" = observe_dataset.base_pubsub_events.oid
+    "observation" = var.datastream.dataset
   }
 
   stage {
-    input    = "events"
-    alias    = "projects"
-    pipeline = <<-EOF
-        filter (string(attributes.data_type) = "cloudresourcemanager.Project")
-        make_col data:parse_json(data)
-        make_col collecting: string(attributes.collecting) 
 
-        make_col createTime:string(data.createTime),
-            lifecycleState:string(data.lifecycleState),
-            name:string(data.name),
-            project_id:string(data.projectId),
-            projectNumber:string(data.projectNumber),
-            parent_id:string(data.parent.id),
-            parent_type:string(data.parent.type)
+    pipeline = <<-EOF
+        filter OBSERVATION_KIND = "gcpprojects"
+        make_col data:FIELDS.project
+
+        make_col 
+          time:BUNDLE_TIMESTAMP,
+          update_time:timestamp_s(int64(data.update_time.seconds)) + if_null(duration(int64(data.update_time.nanos)), 0s),
+          createTime:timestamp_s(int64(data.create_time.seconds)) + if_null(duration(int64(data.create_time.nanos)), 0s),
+            updateTime:timestamp_s(int64(data.update_time.seconds)) + if_null(duration(int64(data.update_time.nanos)), 0s),
+            lifecycleState: case (int64(data.state)=0, "LIFECYCLE_STATE_UNSPECIFIED", int64(data.state)=1,"ACTIVE",int64(data.state)=2,"DELETE_REQUESTED", int64(data.state)=3,"DELETE_IN_PROGRESS", true, 'UNKNOWN') ,
+            project_id:string(data.project_id),
+            parent_id: string(data.parent),
+            display_name:string(data.display_name),
+            etag:string(data.etag)
+
+        set_valid_from options(max_time_diff:${var.max_time_diff}), time
+
+        extract_regex  string(data.name), /^[^\/]+\/(?P<projectNumber>.*)/
 
         make_col deleted: bool(if(lifecycleState != "ACTIVE", true, false))
 
         make_col ttl: case(deleted, 1ns, true, 4h)
 
-        filter collecting = "true"
+
     EOF
   }
 
   stage {
     pipeline = <<-EOF
-      make_resource 
-        createTime,
-        lifecycleState,
-        name,
-        project_id,
-        parent_id,
-        parent_type,
-        primary_key(projectNumber)
-      add_key project_id
-      set_label project_id
+    make_resource 
+      time,
+      update_time,
+      createTime,
+      lifecycleState,
+      project_id,
+      parent_id,
+      display_name,
+      etag,
+      deleted,
+      primary_key(projectNumber),
+      valid_for(ttl)
+
+    add_key project_id
+
+    set_label project_id
+
     EOF
   }
 }
@@ -61,50 +74,50 @@ resource "observe_dataset" "projects_collection_enabled" {
 #   fields    = each.value.fields
 #   label     = each.key
 # }
-resource "observe_dataset" "projects_all" {
-  workspace   = var.workspace.oid
-  name        = format(var.name_format, "Projects All")
-  freshness   = lookup(var.freshness_overrides, "projects", var.freshness_default)
-  description = "This dataset is used to create project resources"
-  inputs = {
-    "events" = observe_dataset.base_pubsub_events.oid
-  }
+# resource "observe_dataset" "projects_all" {
+#   workspace   = var.workspace.oid
+#   name        = format(var.name_format, "Projects All")
+#   freshness   = lookup(var.freshness_overrides, "projects", var.freshness_default)
+#   description = "This dataset is used to create project resources"
+#   inputs = {
+#     "events" = observe_dataset.base_pubsub_events.oid
+#   }
 
-  stage {
-    input    = "events"
-    alias    = "projects"
-    pipeline = <<-EOF
-        filter (string(attributes.data_type) = "cloudresourcemanager.Project")
-        make_col data:parse_json(data)
-        make_col collecting: string(attributes.collecting) 
+#   stage {
+#     input    = "events"
+#     alias    = "projects"
+#     pipeline = <<-EOF
+#         filter (string(attributes.data_type) = "cloudresourcemanager.Project")
+#         make_col data:parse_json(data)
+#         make_col collecting: string(attributes.collecting) 
 
-        make_col createTime:string(data.createTime),
-            lifecycleState:string(data.lifecycleState),
-            name:string(data.name),
-            project_id:string(data.projectId),
-            projectNumber:string(data.projectNumber),
-            parent_id:string(data.parent.id),
-            parent_type:string(data.parent.type)
+#         make_col createTime:string(data.createTime),
+#             lifecycleState:string(data.lifecycleState),
+#             name:string(data.name),
+#             project_id:string(data.projectId),
+#             projectNumber:string(data.projectNumber),
+#             parent_id:string(data.parent.id),
+#             parent_type:string(data.parent.type)
 
-        make_col deleted: bool(if(lifecycleState != "ACTIVE", true, false))
+#         make_col deleted: bool(if(lifecycleState != "ACTIVE", true, false))
 
-        make_col ttl: case(deleted, 1ns, true, 4h)
+#         make_col ttl: case(deleted, 1ns, true, 4h)
 
-    EOF
-  }
+#     EOF
+#   }
 
-  stage {
-    pipeline = <<-EOF
-      make_resource 
-        createTime,
-        lifecycleState,
-        name,
-        project_id,
-        parent_id,
-        parent_type,
-        primary_key(projectNumber)
-      add_key project_id
-      set_label project_id
-    EOF
-  }
-}
+#   stage {
+#     pipeline = <<-EOF
+#       make_resource 
+#         createTime,
+#         lifecycleState,
+#         name,
+#         project_id,
+#         parent_id,
+#         parent_type,
+#         primary_key(projectNumber)
+#       add_key project_id
+#       set_label project_id
+#     EOF
+#   }
+# }
