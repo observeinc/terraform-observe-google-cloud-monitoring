28274af9d00f4790c3a6781b18b5dbd14b007645
Nathan (Dae Hyun) Nam <NathanNam@users.noreply.github.com>
2022-10-05 15:53:11 -0700

    docs: update labels for GCP APP
    docs: update labels for GCP APP

--- sources/pubsub/variables.tf
+++ sources/pubsub/variables.tf
@@ -40,7 +40,7 @@ variable "service_account_private_key_json" {
   type        = string
   description = <<-EOF
     A GCP Service Account should include the following role: Pub/Sub Subscriber (roles/pubsub.subscriber).
-    Please enter the entire JSON string you either generated with Terraform(https://docs.observeinc.com/en/latest/content/integrations/gcp/gcp.html#terraform-service-account-key) 
-    or downloaded from the GCP Console(https://docs.observeinc.com/en/latest/content/integrations/gcp/gcp.html#gcp-console-service-account-key).
+    
+    Please enter the entire JSON string for your service account.
   EOF
 }
