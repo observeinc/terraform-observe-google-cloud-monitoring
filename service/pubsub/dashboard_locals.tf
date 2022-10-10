9bfd2125dec28cb62218c2b1e1be3ffd03f2c5e2
Arthur Dayton <arthur@observeinc.com>
2022-10-10 10:30:14 -0700

    remove resource. ref

--- service/pubsub/dashboard_locals.tf
+++ service/pubsub/dashboard_locals.tf
@@ -1,13 +1,13 @@
 locals {
   workspace                    = var.workspace.oid
   dashboard_name_topic         = format(var.name_format, "Topic Monitoring")
-  pubsub_topics                = resource.observe_dataset.pubsub_topics.id
-  pubsub_topic_metrics         = one(resource.observe_dataset.pubsub_topic_metrics[*].id)
+  pubsub_topics                = observe_dataset.pubsub_topics.id
+  pubsub_topic_metrics         = one(observe_dataset.pubsub_topic_metrics[*].id)
   dashboard_name_subscription  = format(var.name_format, "Subscription Monitoring")
-  pubsub_subscriptions         = resource.observe_dataset.pubsub_subscriptions.id
-  pubsub_subscription_metrics  = one(resource.observe_dataset.pubsub_subscription_metrics[*].id)
+  pubsub_subscriptions         = observe_dataset.pubsub_subscriptions.id
+  pubsub_subscription_metrics  = one(observe_dataset.pubsub_subscription_metrics[*].id)
   dashboard_name_service       = format(var.name_format, "Service Monitoring")
-  pubsub_service               = resource.observe_dataset.pubsub_service.id
-  pubsub_service_api_metrics   = resource.observe_dataset.pubsub_service_api_metrics.id
-  pubsub_service_quota_metrics = resource.observe_dataset.pubsub_service_quota_metrics.id
+  pubsub_service               = observe_dataset.pubsub_service.id
+  pubsub_service_api_metrics   = observe_dataset.pubsub_service_api_metrics.id
+  pubsub_service_quota_metrics = observe_dataset.pubsub_service_quota_metrics.id
 }
