e5cdfb2115c3d0324b35ec94947421b06f579c29
Arthur Dayton <arthur@observeinc.com>
2022-10-10 11:11:56 -0700

    remove test code folder

--- test_code/cloudfunctions/modules/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "function_uri" {
-  value = google_cloudfunctions_function.function.https_trigger_url
-}
--- test_code/cloudfunctions/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "function_uri" {
-  value = google_cloudfunctions_function.function.https_trigger_url
-}
-
-output "function2_uri" {
-  value = module.function2.function_uri
-}
