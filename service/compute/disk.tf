15dc4554cda9ec57d378cd5b14da8e0daaac6007
Arthur Dayton <arthur@observeinc.com>
2022-11-03 15:09:33 -0700

    update asset keys

--- service/compute/disk.tf
+++ service/compute/disk.tf
@@ -14,49 +14,64 @@ resource "observe_dataset" "compute_disk" {
     input    = "events"
     pipeline = <<-EOF
 
-      filter asset_type = "compute.googleapis.com/Disk"
 
-      extract_regex name, /projects\/(?P<project_id>[^\/+]+)\/zones\/(?P<zone>[^\/+]+)\/disks\/(?P<instance_name>[^\/+]+)/
+        filter asset_type = "compute.googleapis.com/Disk"
 
-      extract_regex zone, /(?P<region>[a-z]+[-]+[a-z,0-9]+)/
+        make_col users: data.users
 
-      extract_regex string(data.type), /(?P<type>[^\/]+$)/
+        make_col count_of_users: array_length(users)
 
-      make_col
-        assetInventoryName:string(name),
-        instance_id:string(data.id),
-        instance_key: strcat(project_id,":",instance_name),
-        status:if(deleted=true, "DELETED",string(data.status)),
-        description: string(data.description),
-        creationTime: format_time(parse_isotime(string(data.creationTimestamp)), 'MON DD, YYYY HH24:MI:SS'),
-        labels: string(data.labels),
-        tags: string(data.tags)
+        flatten_single users
+
+        extract_regex string(_c_users_value), /projects\/(?P<compute_instance_key_base>.*)/
+        
+        extract_regex name, /(?P<disk_instance_name>[^\/]+$)/
+
+        make_col computeInstanceAssetKey: string_concat("//compute.googleapis.com/projects/",compute_instance_key_base)
+        // ex - //compute.googleapis.com/projects/content-testpproj-stage-1/zones/us-central1-b/instances/test-stg-instance-ubuntu-20-04-lts-57
+
+        extract_regex name, /projects\/(?P<project_id>[^\/+]+)\/zones\/(?P<zone>[^\/+]+)\/disks\/(?P<disk_instance_name>[^\/+]+)/
+
+        extract_regex zone, /(?P<region>[a-z]+[-]+[a-z,0-9]+)/
+
+        extract_regex string(data.type), /(?P<type>[^\/]+$)/
+
+        
+
+        make_col
+          computeDiskInstanceAssetKey:string(name), // ex - //compute.googleapis.com/projects/content-testpproj-stage-1/zones/us-west1-b/disks/gke-test-stg-gke-test-stg-gke-node-po-d4256cbb-1crf
+          disk_id:string(data.id),
+          status:if(deleted=true, "DELETED",string(data.status)),
+          description: string(data.description),
+          creationTime: format_time(parse_isotime(string(data.creationTimestamp)), 'MON DD, YYYY HH24:MI:SS'),
+          labels: string(data.labels),
+          tags: string(data.tags)
 
 
-      make_col architecture:string(data.architecture),
-          guestOsFeatures:data.guestOsFeatures,
-          id:string(data.id),
-          labelFingerprint:string(data.labelFingerprint),
-          lastAttachTimestamp: format_time(parse_isotime(string(data.lastAttachTimestamp)), 'MON DD, YYYY HH24:MI:SS'),
-          licenseCodes:data.licenseCodes,
-          licenses:data.licenses,
-          name: name,
-          physicalBlockSizeBytes:string(data.physicalBlockSizeBytes),
-          selfLink:string(data.selfLink),
-          sizeGb:string(data.sizeGb),
-          sourceImage:string(data.sourceImage),
-          sourceImageId:string(data.sourceImageId),
-          status:string(data.status),
-          users:data.users
+        make_col architecture:string(data.architecture),
+            guestOsFeatures:data.guestOsFeatures,
+            id:string(data.id),
+            labelFingerprint:string(data.labelFingerprint),
+            lastAttachTimestamp: format_time(parse_isotime(string(data.lastAttachTimestamp)), 'MON DD, YYYY HH24:MI:SS'),
+            licenseCodes:data.licenseCodes,
+            licenses:data.licenses,
+            name: name,
+            physicalBlockSizeBytes:string(data.physicalBlockSizeBytes),
+            selfLink:string(data.selfLink),
+            sizeGb:string(data.sizeGb),
+            sourceImage:string(data.sourceImage),
+            sourceImageId:string(data.sourceImageId),
+            status:string(data.status)
 
     EOF
   }
 
   stage {
     pipeline = <<-EOF
-      make_resource options(expiry:${var.max_expiry}),
-        instance_name,
-        instance_id,
+      make_resource options(expiry:4h),
+        disk_instance_name,
+        disk_id,
+        computeInstanceAssetKey,
         architecture,
         guestOsFeatures,
         licenses,
@@ -69,7 +84,6 @@ resource "observe_dataset" "compute_disk" {
         sourceImage,
         sourceImageId,
         type,
-        users,
         project_id, 
         region, 
         zone,
@@ -77,32 +91,15 @@ resource "observe_dataset" "compute_disk" {
         tags,
         ttl,
         deleted,
-        primary_key(id),
+        primary_key(computeDiskInstanceAssetKey),
         valid_for(ttl)
 
-      add_key instance_id
-      set_label id
+      add_key computeInstanceAssetKey
+      set_label disk_instance_name
 
-      // add_key project_id, region, zone
+      add_key project_id
+
     EOF
   }
 }
 
-resource "observe_link" "project_disk" {
-  for_each = {
-    "Projects" = {
-      target = var.google.projects.oid
-      fields = ["project_id"]
-    }
-    "Compute" = {
-      target = observe_dataset.compute_instance.oid
-      fields = ["instance_id"]
-    }
-  }
-
-  workspace = var.workspace.oid
-  source    = observe_dataset.compute_disk.oid
-  target    = each.value.target
-  fields    = each.value.fields
-  label     = each.key
-}
