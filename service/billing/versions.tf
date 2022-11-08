631bfc8fd95b0d5267ea15744cc3ba7a97000ed2
Arthur Dayton <arthur@observeinc.com>
2022-11-08 08:11:14 -0800

    update versions

--- service/billing/versions.tf
+++ service/billing/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     observe = {
       source  = "terraform.observeinc.com/observeinc/observe"
-      version = ">= 0.9.2, <= 0.12.1"
+      version = ">=0.11.1, <= 0.12.1"
     }
   }
   required_version = ">= 1.2.0"
