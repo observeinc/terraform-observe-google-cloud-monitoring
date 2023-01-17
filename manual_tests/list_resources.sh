1a927369ef9e0ea1b20b6d02d35271a9162661ba
Arthur Dayton <arthur@observeinc.com>
2023-01-17 13:51:25 -0800

    add manual testing script

---
+++ manual_tests/list_resources.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+array=( 'sql instances list' 'compute instances list' 'container clusters list' 
+'functions list' 'storage buckets list' 'alpha bq datasets list --all' 
+'pubsub topics list' 'pubsub subscriptions list'
+'compute url-maps list' 'scheduler jobs list --location=us-west1')
+
+for var in "${array[@]}"
+do
+    gloudstr="gcloud ${var} --format json| jq -r '. | length'";
+    echo "####################"
+    echo "Count of ${var} = $(eval "$gloudstr")";
+
+    if [[ "${var}" == 'alpha bq datasets list --all' ]]; then
+        datasetinstances="gcloud ${var} --format json| jq -r '\"    \" + .[].id'"
+        echo " List of instances = ";
+        eval "$datasetinstances";
+    else
+        gcloudinstances="gcloud ${var} --format json| jq -r '\"    \" + .[].name'"
+        echo " List of instances = ";
+        eval "$gcloudinstances";
+    fi
+    echo "####################"
+    echo ""
+done
+
+# gcloud alpha bq tables list --dataset test_eng_dataset
+
+
+# https://cloud.google.com/sdk/gcloud/reference/compute/url-maps
+# https://cloud.google.com/sdk/gcloud/reference/alpha/bq/datasets/list
+# https://cloud.google.com/sdk/gcloud/reference/alpha/bq/tables/list
+# https://cloud.google.com/sdk/gcloud/reference/container/clusters/list
+# https://cloud.google.com/sdk/gcloud/reference/compute/instances/list
+# https://cloud.google.com/sdk/gcloud/reference/sql/instances/list
+# https://cloud.google.com/sdk/gcloud/reference/config/set
+# https://cloud.google.com/sdk/gcloud/reference/scheduler/jobs/list
