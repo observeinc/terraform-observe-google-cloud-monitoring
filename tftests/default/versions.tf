301d9340977235def68f5d7efd858a0c23f905ec
Arthur Dayton <arthur@observeinc.com>
2023-02-06 10:17:00 -0800

    update version to 13.2 - remove deprecated folders

--- tftests/default/versions.tf
+++ tftests/default/versions.tf
@@ -5,7 +5,7 @@ terraform {
     }
     observe = {
       source  = "terraform.observeinc.com/observeinc/observe"
-      version = "0.13.1"
+      version = "0.13.2"
     }
   }
   required_version = ">= 1.0.0"
