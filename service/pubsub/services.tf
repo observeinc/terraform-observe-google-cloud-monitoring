babb2deae56b0cb062cdc7ca53b6ab5a31bc6cb7
Arthur Dayton <arthur@observeinc.com>
2023-02-07 15:38:37 -0800

    update service metrics

--- service/pubsub/services.tf
+++ service/pubsub/services.tf
@@ -129,6 +129,68 @@ if(contains(var.metric_launch_stages, options.launchStage) && options.metricBin 
 
     EOF
 }
+# The terraform below dynamically writes set_metric statements in opal
+# This loops through the local.metrics_definitions map using for
+# [for metric, options in local.metrics_definitions :
+
+# metric is the key (avg_ttl) and options is the value (all the stuff between {})
+/* Example metric in local.metrics_definitions
+        avg_ttl = {
+            type               = "gauge"
+            description        = <<-EOF
+                            Average TTL for keys in this database.
+                        EOF
+            launchStage        = "GA"
+            rollup             = "avg"
+            aggregate          = "sum"
+            metricCategory     = "none"
+            google_metric_path = "redis.googleapis.com/keyspace/avg_ttl"
+            label              = "Average TTL"
+            unit               = "ms"
+            metricBin          = "keyspace"
+            valuetype          = "DOUBLE"
+
+        }
+        */
+
+# We filter the outer for loop checking whether options.launchStage is in the array defined by var.metric_launch_stages 
+# in the inner for loop we iterate through the fields in the options objects and check if the field is in the array defined by var.metric_interface_fields
+##  and if so 
+/* Example output
+  set_metric options(
+    aggregate: "sum",
+    description: "Average TTL for keys in this database.\n",
+    rollup: "avg",
+    type: "gauge",
+    unit: "ms"
+    ), "avg_ttl"
+    
+  */
+
+stage {
+  pipeline = <<-EOF
+      interface "metric", metric:metric, value:value
+      ${join("\n\n",
+  [for metric, options in local.merged_metrics_definitions_service :
+    indent(2,
+      # format takes result of join / forloop and metric as inputs
+      format("set_metric options(\n%s\n), %q",
+        join(",\n",
+          [for optionFieldName, optionFieldNameValue in options :
+            optionFieldName == "interval" ? format("%s: %s", optionFieldName, optionFieldNameValue) :
+            # format takes optionFieldName and optionFieldNameValue as inputs
+            format("%s: %q", optionFieldName, optionFieldNameValue)
+            if contains(var.metric_interface_fields, optionFieldName) # filters inner for loop
+          ]                                                           # end of inner for loop
+        ),                                                            # end of inner join statement
+    replace(replace(options.google_metric_path, "serviceruntime.googleapis.com/", ""), "/", "_")))
+if(contains(var.metric_launch_stages, options.launchStage) && options.metricBin == "api")])}
+                                                                 # filters outer for loop
+    ]                                                            # end of outer for loop and join statement
+)}  
+
+    EOF
+}
 }
 
 resource "observe_dataset" "pubsub_service_quota_metrics" {
