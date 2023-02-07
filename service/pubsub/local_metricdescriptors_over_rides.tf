7697ee7ac1c01e896931c9814e55ed0b2b88f55d
Arthur Dayton <arthur@observeinc.com>
2023-02-07 15:38:24 -0800

    update pubsub metrics

---
+++ service/pubsub/local_metricdescriptors_over_rides.tf
@@ -0,0 +1,39 @@
+locals {
+  override_keys         = keys(local.metrics_definitions_overrides)
+  override_keys_service = keys(local.metrics_definitions_overrides_service)
+  # if local.metrics_definitions has an overide value in local.metrics_definitions_overrides then use value from local.metrics_definitions_overrides
+  merged_metrics_definitions = { for key1, value1 in local.metrics_definitions : # loop
+    key1 => {
+      for key2, value2 in value1 :
+      key2 => contains(local.override_keys, key1) ?                     # check if metric key in override map
+      contains(keys(local.metrics_definitions_overrides[key1]), key2) ? # check if property in override map for given key
+      local.metrics_definitions_overrides[key1][key2] : value2          # use if present otherwise use existing
+      : value2                                                          # otherwise use existing
+    }
+  }
+
+  merged_metrics_definitions_service = { for key1, value1 in local.service_metrics_definitions : # loop
+    key1 => {
+      for key2, value2 in value1 :
+      key2 => contains(local.override_keys_service, key1) ?                     # check if metric key in override map
+      contains(keys(local.metrics_definitions_overrides_service[key1]), key2) ? # check if property in override map for given key
+      local.metrics_definitions_overrides_service[key1][key2] : value2          # use if present otherwise use existing
+      : value2                                                                  # otherwise use existing
+    }
+  }
+
+  metrics_definitions_overrides = {
+    example = {
+      rollup = "rate"
+      unit   = "second"
+    }
+  }
+
+  metrics_definitions_overrides_service = {
+    example = {
+      rollup = "rate"
+      unit   = "second"
+    }
+  }
+}
+
