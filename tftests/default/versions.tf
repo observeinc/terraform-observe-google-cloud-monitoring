d82b18aba9f85fcafd42b7e1fd9013f23a80d009
Ben Drucker <bvdrucker@gmail.com>
2023-02-24 13:41:12 -0800

    test with 0.13.6 RC

--- tftests/default/versions.tf
+++ tftests/default/versions.tf
@@ -5,7 +5,7 @@ terraform {
     }
     observe = {
       source  = "terraform.observeinc.com/observeinc/observe"
-      version = "~>0.13"
+      version = "0.13.5-2-g94c9a498"
     }
   }
   required_version = ">= 1.0.0"
