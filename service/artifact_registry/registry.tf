80205f77bd35a2ef6ee6a0afbb35904727d52047
Arthur Dayton <arthur@observeinc.com>
2023-02-28 16:52:53 -0800

    key on registry

--- service/artifact_registry/registry.tf
+++ service/artifact_registry/registry.tf
@@ -14,7 +14,7 @@ resource "observe_dataset" "artifact_registry_instances" {
         make_col
             description:string(data.description),
             format:string(data.format),
-            instance_pkey:string(data.name)
+            registry_pkey:string(data.name)
 
         extract_regex name, /repositories\/(?P<displayName>.*)/
         EOF
@@ -32,7 +32,7 @@ resource "observe_dataset" "artifact_registry_instances" {
         format,
         update_time,
         ttl,
-        primary_key(instance_pkey)
+        primary_key(registry_pkey)
 
       EOF
   }
@@ -52,13 +52,24 @@ resource "observe_dataset" "artifact_registry_images" {
   stage {
     pipeline = <<-EOF
         filter asset_type = "artifactregistry.googleapis.com/DockerImage"
-        make_col imageSizeBytes:string(data.imageSizeBytes),
+        make_col imageSizeBytes:float64(data.imageSizeBytes),
             mediaType:string(data.mediaType),
-            name_1:string(data.name),
             tags:data.tags,
-            uploadTime:string(data.uploadTime),
+            upload_time:parse_isotime(data.uploadTime),
             uri:string(data.uri)
 
+        extract_regex name, /(?P<registry_pkey>.*)\/.*\//
+        extract_regex name, /@.*:(?P<sha>.*)/
+
+        extract_regex name, /repositories\/.*\/(?P<image_name>.*).*@/
+
+        make_col dur_since_ms: float64(time-upload_time)
+        make_col dur_since_log: log(10,dur_since_ms)*100
+        make_col dur_since_float: float64(dur_since_log)
+        make_col dur_since: time-upload_time
+        make_col adj_time: time-duration_ms(dur_since_float)
+
+        set_valid_from options(max_time_diff:4h), adj_time
         EOF
   }
 
@@ -66,6 +77,31 @@ resource "observe_dataset" "artifact_registry_images" {
 
     pipeline = <<-EOF
 
+      make_resource options(expiry:${var.max_expiry_duration}),
+        adj_time,
+        time,
+        update_time,
+        upload_time,
+        project_id,
+        image_name,
+        tags,
+        sha,
+        imageSizeBytes,
+        mediaType,
+        uri,
+        ttl,
+        registry_pkey,
+        primary_key(name)
+
+      add_key registry_pkey
       EOF
   }
+}
+
+resource "observe_link" "images_to_artifact_registry" {
+  workspace = local.workspace.oid
+  target    = observe_dataset.artifact_registry_instances.oid
+  source    = observe_dataset.artifact_registry_images.oid
+  fields    = ["registry_pkey"]
+  label     = "Artifact Registry"
 }
