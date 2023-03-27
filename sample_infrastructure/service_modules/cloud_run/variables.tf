df2b925b93480a8ba632efec2a3c308242218e0c
Abhishek Rao <abhishek.rao@observeinc.com>
2023-03-27 19:10:44 -0400

    feat: use fakeService and add simple automation for load testing

--- sample_infrastructure/service_modules/cloud_run/variables.tf
+++ sample_infrastructure/service_modules/cloud_run/variables.tf
@@ -12,3 +12,9 @@ variable "name_format" {
   type        = string
   description = "Format string to use for infra names."
 }
+
+variable "always_run_load_tests" {
+  type        = bool
+  description = "Configures whether to run a load test in every deploy of this resource"
+  default     = false
+}
