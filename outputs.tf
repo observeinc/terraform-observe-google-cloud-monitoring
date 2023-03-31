fea5855639781eb7971308acac451e9e6bc9bfde
Abhishek Rao <abhishek.rao@observeinc.com>
2023-03-31 18:44:30 -0400

    feat: expiry logic for services and revisions

--- outputs.tf
+++ outputs.tf
@@ -38,6 +38,10 @@ output "cloud_functions" {
   value = local.enable_service_cloudfunctions ? module.cloudfunctions[0] : null
 }
 
+output "cloud_run" {
+  value = local.enable_service_cloudrun ? module.cloudrun[0] : null
+}
+
 output "cloud_sql" {
   value = local.enable_service_cloudsql ? module.cloudsql[0] : null
 }
--- service/cloudrun/outputs.tf
+++ service/cloudrun/outputs.tf
@@ -1,11 +1,7 @@
-# output "function" {
-#   value = observe_dataset.cloud_functions_instances
-# }
+output "cloud_run_service_instances" {
+  value = observe_dataset.cloud_run_service_instances
+}
 
-# output "function_logs" {
-#   value = observe_dataset.cloud_functions_function_logs
-# }
-
-# output "function_metrics" {
-#   value = local.enable_metrics ? observe_dataset.cloud_functions_metrics[0] : null
-# }
+output "cloud_run_revision_instances" {
+  value = observe_dataset.cloud_run_revision_instances
+}
