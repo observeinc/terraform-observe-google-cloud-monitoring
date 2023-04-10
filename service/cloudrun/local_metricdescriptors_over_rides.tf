d7f56975f88250babebdb845181cc1440899342b
obs-gh-abhishekrao <122921682+obs-gh-abhishekrao@users.noreply.github.com>
2023-04-10 10:29:33 -0400

    feat: include interval in cloud run metric definition (#161)

--- service/cloudrun/local_metricdescriptors_over_rides.tf
+++ service/cloudrun/local_metricdescriptors_over_rides.tf
@@ -2,15 +2,14 @@ locals {
   override_keys = keys(local.metrics_definitions_overrides)
   # if local.metrics_definitions has an overide value in local.metrics_definitions_overrides then use value from local.metrics_definitions_overrides
   merged_metrics_definitions = { for key1, value1 in local.metrics_definitions : # loop
-    key1 => {
+    key1 => merge({
       for key2, value2 in value1 :
       key2 => contains(local.override_keys, key1) ?                     # check if metric key in override map
       contains(keys(local.metrics_definitions_overrides[key1]), key2) ? # check if property in override map for given key
       local.metrics_definitions_overrides[key1][key2] : value2          # use if present otherwise use existing
       : value2                                                          # otherwise use existing
-    }
+    }, { interval = "1m" })
   }
-
 
   # This will change the rollup and unit properties of the cpu_utilization metric defined in local.metrics_definitions 
 
