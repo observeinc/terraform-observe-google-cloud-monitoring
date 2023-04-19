fdd86c9e0130c7cb6fbd982d69d5e4a9d87af688
Arthur Dayton <arthur@observeinc.com>
2023-04-18 18:04:09 -0700

    remove frame that is causing bucketing issue

--- service/cloudsql/dashboard_locals.tf
+++ service/cloudsql/dashboard_locals.tf
@@ -1,11 +1,13 @@
 locals {
-  workspace                           = var.workspace.oid
-  dashboard_name_monitoring           = format(var.name_format, "Monitoring")
-  dashboard_name_singleton            = format(var.name_format, "Instance")
-  cloud_sql_instance                  = observe_dataset.cloud_sql_instance.id
-  cloud_sql_metrics                   = one(observe_dataset.cloud_sql_metrics[*].id)
-  cloud_sql_metrics_combo             = one(observe_dataset.cloud_sql_metrics_combo[*].id)
-  cloud_sql_logs_error                = observe_dataset.cloud_sql_logs_error.id
-  cloud_sql_metrics_wide              = one(observe_dataset.cloud_sql_metrics_wide[*].id)
-  cloud_sql_logs_postgres_data_access = observe_dataset.postgres_data_access_logs.id
+  workspace                                  = var.workspace.oid
+  cloud_sql_monitoring_dashboard_name        = format(var.name_format, "Monitoring")
+  cloud_sql_monitoring_dashboard_description = "CloudSQL Instances Monitoring Dashboard"
+  cloud_sql_monitoring_dashboard_enable      = 1
+  dashboard_name_singleton                   = format(var.name_format, "Instance")
+  cloud_sql_instance                         = observe_dataset.cloud_sql_instance.id
+  cloud_sql_metrics                          = one(observe_dataset.cloud_sql_metrics[*].id)
+  cloud_sql_metrics_combo                    = one(observe_dataset.cloud_sql_metrics_combo[*].id)
+  cloud_sql_logs_error                       = observe_dataset.cloud_sql_logs_error.id
+  cloud_sql_metrics_wide                     = one(observe_dataset.cloud_sql_metrics_wide[*].id)
+  cloud_sql_logs_postgres_data_access        = observe_dataset.postgres_data_access_logs.id
 }
