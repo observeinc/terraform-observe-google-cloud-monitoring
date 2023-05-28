59ada18faf6b26df3827903213eba0d5b307fea1
Arthur Dayton <arthur@observeinc.com>
2023-05-28 09:00:53 -0700

    fix for precommit

--- customer_examples/terraform_observe_pollers/versions.tf
+++ customer_examples/terraform_observe_pollers/versions.tf
@@ -1,9 +1,16 @@
 terraform {
   required_providers {
     observe = {
-      source = "terraform.observeinc.com/observeinc/observe"
+      source  = "terraform.observeinc.com/observeinc/observe"
+      version = "~> 0.13"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "<= 4.37.0"
     }
   }
+  required_version = ">= 1.3.0"
+
 }
 
 provider "observe" {
